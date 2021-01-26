48c5f7aaa5b68399c9c9656f7d0fa76f992285cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-26 16:28:11 -0500

    added properties to space ship

--- src/SpaceShip.h
+++ src/SpaceShip.h
@@ -21,9 +21,15 @@ public:
 	// getters and setters
 	void setDestination(glm::vec2 destination);
 	void setMaxSpeed(float speed);
+	glm::vec2 getOrientation() const;
 	void setOrientation(glm::vec2 orientation);
-	void setRotation(float angle);
 	float getRotation() const;
+	void setRotation(float angle);
+	float getTurnRate() const;
+	void setTurnRate(float rate);
+	float getAccelerationRate() const;
+	void setAccelerationRate(float rate);
+	
 	
 private:
 
@@ -32,6 +38,8 @@ private:
 	glm::vec2 m_orientation;
 	float m_rotationAngle;
 	float m_maxSpeed;
+	float m_turnRate;
+	float m_accelerationRate;
 
 	// private function
 	void m_Move();
